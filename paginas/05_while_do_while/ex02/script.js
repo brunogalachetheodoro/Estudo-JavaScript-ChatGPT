16f5ab85f346ac81e9373b9c4099863d32d0781f
Bruno Galache Theodoro <bruno.hgt2@gmail.com>
2024-11-19 05:24:27 -0300

    finalizado ex while.. do while

--- paginas/05_while_do_while/ex02/script.js
+++ paginas/05_while_do_while/ex02/script.js
@@ -1,6 +1,15 @@
-let senha = 1234
+let senha = 1234;
 
 do {
-    let senhaInserida = parseInt(prompt('Digite uma senha de 4 digitos: ').value)
-    document.getElementById('resposta').innerHTML = 'Senha errada! Tente novamente'
-} while (senhaInserida =! 1234)
+    let senhaInserida = parseInt(prompt('Digite uma senha de 4 digitos: '))
+    if(senhaInserida !== senha) {
+        document.getElementById('resposta').innerHTML = 'Senha errada! Tente novamente.'
+
+        setTimeout(() => {
+            location.reload();
+        },1000);
+    } else {
+        document.getElementById('resposta').innerHTML = 'Senha Correta! Acesso Liberado.'
+    }
+} while (senhaInserida !== senha)
+
--- paginas/05_while_do_while/ex03/script.js
+++ paginas/05_while_do_while/ex03/script.js
@@ -1,10 +1,12 @@
 let numeroInserido = parseInt(prompt('Insira um número: '))
 let contador = 1
 let soma = 0
+
 while(contador <= numeroInserido) {
     if(contador % 2 == 0) {
+        document.getElementById('resposta').innerHTML += `${soma} + ${contador} = `
         soma += contador
-        document.getElementById('resposta').innerHTML += `${soma} + ${contador} = ${soma},`
+        document.getElementById('resposta').innerHTML += `${soma}<br> `
     }
     contador++
 }
